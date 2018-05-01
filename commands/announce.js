87ed2bd2a15ec222495a97cf81d26d49e764d89c
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-01 19:58:26 +0100

    Update announce.js

--- commands/announce.js
+++ commands/announce.js
@@ -1,23 +1,19 @@
 exports.run = (Discord, client, message, args) => {
 
-	
-if (message.member.hasPermission("ADMINISTRATOR")) {
+       if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("You need the ADMINISTRATOR permission to run this comamnd!")
 		   const color = args[0]
-				
+		   let title = args[0];
 		   const text = args.slice(1).join(" ");
-		   const title = args.slice(0).join(" ");
 		   const author = message.author;
 		   if (text.length < 1) return message.channel.send("Can not announce nothing");
 		   //const colour = args.slice(2).join("");
 		   const embed = new Discord.RichEmbed()
-		   .setColor("#FF0000")
-		   .setThumbnail("https://t7.rbxcdn.com/fe33fe198b2630c5d582ac2e039e96cf")
+		   .setColor("#00ff00")
+		   .setThumbnail(client.user.avatarURL)
 		   .setTitle(":mega: Announcement: " + title)
-		   .setDescription(text)
-		   .setFooter("An announcment from " + author + " made at ")
+		   .setDescription("**Announced by: " + message.author + "**\n\n" + text + "\n")
+		   .setFooter("An announcment made at ")
 		   .setTimestamp()
-		   message.channel.send("@everyone");
-		   message.channel.send("Announcement from: " + author);
 		   message.channel.send({embed})
-	   }
+	   
 }
