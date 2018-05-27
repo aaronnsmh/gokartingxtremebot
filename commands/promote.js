0065e77b35aba7abfae98fd553bc0d8d0d4e4d98
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-27 11:11:42 +0100

    Update promote.js

--- commands/promote.js
+++ commands/promote.js
@@ -1,9 +1,8 @@
 var roblox = require('noblox.js');
 
-exports.run = (Discord, client, message, args) => {
-if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("No can do pal! Only BoX can run this command..");
-	
-	
+exports.run = (Discord, client, message, args) => {;
+let allowedRole = message.guild.roles.find("name", "Bot Ranker");	
+if(!message.member.roles.has(allowedRole.id)) return message.channel.send("No can do pal! Only users with the `Bot Ranker` role can run this command.");
 var groupId = 2633056;
 var maximumRank = 6;
 
